c4de483df25e8b7e0bf0453e003f1a2985475ce5
Andi Arnautu <worg@pop-os.localdomain>
2020-03-09 19:57:14 +0200

    Finished block problem

--- lab4/block_problem.py
+++ lab4/block_problem.py
@@ -11,6 +11,9 @@ class NodeInGraph:
     def __init__(self, state, parent):
         self.state = state
         self.parent = parent
+        self.g = 0  #  Distance to start node
+        self.h = 0  #  Distance to end node
+        self.f = 0  #  Total distance
 
 
     def get_path(self):
@@ -20,14 +23,52 @@ class NodeInGraph:
         while node is not None:
             node_list.append(node)
             node = node.parent
-        return node_list.reverse()
+
+        path = ""
+        for i in range(len(node_list) -1, -1, -1):
+            path += node_list[i].__repr__()
+        return path
+
+
+    def compute_heuristic(self, end_node):
+        stack_id = {}
+        for i in range(len(self.state)):
+            for element in self.state[i]:
+                stack_id[element] = i
+
+        heuristic_cost = 0
+        for i in range(len(end_node.state)):
+            for element in end_node.state[i]:
+                heuristic_cost += abs(i - stack_id[element])
+        return heuristic_cost
 
 
     def __repr__(self):
         """ Format the node data into something readable """
-        return self.state
+        string = "State (top of each stack is at the left):\n"
+
+        stack_list = []
+        for i in range(len(self.state)):
+            stack_string = "["
+            for j in range(len(self.state[i])):
+                stack_string += str(self.state[i][j])
+                if j < len(self.state[i]) - 1:
+                    stack_string += " "
+            stack_string += "]\n"
+            stack_list.append(stack_string)
 
 
+        for stack_string in stack_list[::-1]:
+            string += stack_string
+        string += "\n"
+        return string
+
+
+    def __eq__(self, other):
+        return self.state == other.state
+
+    def __lt__(self, other):
+        return self.f < other.f
 
 
 class Graph:
@@ -46,18 +87,65 @@ class Graph:
             #  Try to move the top of the i-th stack
             if len(node.state[i]) > 0:
                 state_copy = copy.deepcopy(node.state)
-                aux_node = NodeInGraph(info_copy, node)
+                aux_node = NodeInGraph(state_copy, node)
                 element = aux_node.state[i].pop(0)
 
                 for j in range(self.size):
                     if i != j:
                         state_new = copy.deepcopy(aux_node.state)
+                        next_node = NodeInGraph(state_new, node)
+                        next_node.state[j].insert(0, element)
+                        expand_list.append(next_node)
+        return expand_list
 
 
 
+def astar_search(graph: Graph):
+    open_nodes = []
+    closed_nodes = []
+
+    #  Create the start and end node
+    start_node = NodeInGraph(graph.start_config, None)
+    end_node = NodeInGraph(graph.end_config, None)
+    open_nodes.append(start_node)
+
+    while len(open_nodes) > 0:
+        open_nodes.sort()
+        current_node = open_nodes.pop(0)
+        closed_nodes.append(current_node)
+
+        #  Check if we reached the goal and return the path
+        if current_node == end_node:
+            return current_node.get_path()
 
 
+        #  Get neighbours
+        neighbours = graph.expand(current_node)
+        for next_node in neighbours:
+            if next_node in closed_nodes:
+                continue
+            #  Calculate full path cost
+            next_node.g = current_node.g + 1
+            next_node.h = next_node.compute_heuristic(end_node)
+            next_node.f = next_node.g + next_node.h
+
+            #  Check if the neighbour is in open list and if it has a lower f value
+            worth_adding = True
+            for node in open_nodes:
+                if next_node == node and next_node.f > node.f:
+                    wort_adding = False
+                    break
+
+            if worth_adding:
+                open_nodes.append(next_node)
+
+    return None
 
 
 start_config = [["a"], ["b", "c"], ["d"]]
 end_config = [["c", "b"], [], ["a", "d"]]
+graph = Graph(start_config, end_config)
+
+result = astar_search(graph)
+print(type(result))
+print(result)
